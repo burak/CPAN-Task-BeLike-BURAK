d09743fe8dfc523f6bb954caa3c28f4465a14da9
Burak Gürsoy <burak@cpan.org>
2011-02-20 02:57:23 +0100

    Init

---
+++ lib/Task/BeLike/BURAK.pm
@@ -0,0 +1,92 @@
+package Task::BeLike::BURAK;
+use strict;
+use warnings;
+use vars qw( $VERSION );
+
+$VERSION = '0.10';
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Task::Lingua::Any::Numbers - Installs all modules I (BURAK) like
+
+=head1 SYNOPSIS
+
+None.
+
+=head1 DESCRIPTION
+
+This is a tool to install module I use.
+
+=head1 PREREQUISITIES
+
+=head2 MY MODULES
+
+=head3 L<Acme::CPANAuthors::Turkish>
+
+=head3 L<App::Sys::Info>
+
+=head3 L<CGI::Auth::Basic>
+
+=head3 L<Device::CableModem::Motorola::SB4200>
+
+=head3 L<GD::SecurityImage>
+
+=head3 L<GD::Thumbnail>
+
+=head3 L<Lingua::TR::ASCII>
+
+=head3 L<MP3::M3U::Parser>
+
+=head3 L<Parse::HTTP::UserAgent>
+
+=head3 L<PHP::Session::DBI>
+
+=head3 L<Scalar::Util::Reftype>
+
+=head3 L<Task::Lingua::Any::Numbers>
+
+=head3 L<Test::Sys::Info>
+
+=head3 L<Text::Template::Simple>
+
+=head3 L<Time::Elapsed>
+
+=head3 L<Win32::ASP::CGI>
+
+System specific.
+
+=head2 UTILITY
+
+=head3 L<criticism>
+
+=head3 L<Devel::Size>
+
+=head3 L<Perl::Tidy>
+
+=head3 L<Test::Pod>
+
+=head3 L<Test::Pod::Coverage>
+
+=head3 L<Text::Table>
+
+=head2 GUI
+
+=head3 L<Tk>
+
+=head2 WEB
+
+=head3 L<HTML::Parser>
+
+=head3 L<Mojolicious>
+
+=head2 SYSTEM SPECIFIC
+
+=head3 L<Win32::API>
+
+=cut
