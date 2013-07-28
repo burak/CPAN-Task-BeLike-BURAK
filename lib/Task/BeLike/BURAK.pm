9b4176ac7f3350e444f6cef29921b581a60fcef3
Burak Gürsoy <burak@cpan.org>
2013-07-28 08:32:08 +0200

    Add some, remove some. Get rid of the unmaintainable pod

--- lib/Task/BeLike/BURAK.pm
+++ lib/Task/BeLike/BURAK.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use vars qw( $VERSION );
 
-$VERSION = '0.16';
+$VERSION = '0.17';
 
 1;
 
@@ -25,90 +25,6 @@ This is a tool to install module I use.
 
 =head1 PREREQUISITIES
 
-=head2 DATE MANIPULATION
-
-=head3 L<DateTime>
-
-=head3 L<DateTime::Format::W3CDTF>
-
-=head3 L<DateTime::Format::Mail>
-
-=head2 MY MODULES
-
-=head3 L<Acme::CPANAuthors::Turkish>
-
-=head3 L<App::Sys::Info>
-
-=head3 L<CGI::Auth::Basic>
-
-=head3 L<Device::CableModem::Motorola::SB4200>
-
-=head3 L<GD::SecurityImage>
-
-=head3 L<GD::Thumbnail>
-
-=head3 L<Lingua::TR::ASCII>
-
-=head3 L<MP3::M3U::Parser>
-
-=head3 L<Parse::HTTP::UserAgent>
-
-=head3 L<PHP::Session::DBI>
-
-=head3 L<Scalar::Util::Reftype>
-
-=head3 L<Task::Lingua::Any::Numbers>
-
-=head3 L<Test::Sys::Info>
-
-=head3 L<Text::Template::Simple>
-
-=head3 L<Time::Elapsed>
-
-=head3 L<Win32::ASP::CGI>
-
-System specific.
-
-=head2 UTILITY
-
-=head3 L<criticism>
-
-=head3 L<Devel::Size>
-
-=head3 L<File::HomeDir>
-
-=head3 L<Perl::Tidy>
-
-=head3 L<Software::License>
-
-=head3 L<Task::CPAN::Reporter>
-
-=head3 L<Term::ReadLine::Perl>
-
-=head3 L<Test::Pod>
-
-=head3 L<Test::Pod::Coverage>
-
-=head3 L<Text::Table>
-
-=head2 WEB
-
-=head3 L<HTML::Parser>
-
-=head3 L<LWP::UserAgent>
-
-=head3 L<LWP::Protocol::https>
-
-=head3 L<Mojolicious>
-
-=head2 SYSTEM SPECIFIC
-
-=head3 L<Win32::API>
-
-=head2 XML
-
-=head3 L<XML::RSS>
-
-=head3 L<XML::Simple>
+See the meta files in the distribution.
 
 =cut
